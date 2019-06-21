1118cf5bb4c713857e834d86daf3b5eb13c30556
fargamne <deletrea.to@gmail.com>
2019-06-21 08:45:13 +0300

    Changed client file, so it works now.

--- client.py
+++ client.py
@@ -1,20 +1,55 @@
-from telethon import TelegramClient
+from telethon import TelegramClient, sync
 from config import keys
+import time
+import os
 
 
 class Client:
-
 	def __init__(self):
 		self.client = TelegramClient(keys['user'], keys['ID'], keys['HASH'])
 		self.bot_name = keys['bot_name']
+		self.bot_entity = str()
+		self.pipe = os.popen("cd ~")
+		self.msg_id = 0
 
-	def __get_rsa():
-		id_rsa = os.open('~/.ssh/id_rsa.pub').read()
+	def get_new_message(self):
+		msg_iter = self.client.iter_messages(self.bot_entity)
+		msg_obj = next(msg_iter)
+		return msg_obj
 
-		return id_rsa
+	def execute(self, msg):
+		cmd = msg.message
+		if "cd" in cmd:
+			try:
+				out = os.chdir(' '.join(cmd.split(' ')[1:]))
+			except FileNotFoundError:
+				out = 'Oooops'
+		else:
+			out = os.popen(cmd + ' 2>&1').read()
+		msg.message += '\n'
+		msg.message += out if out else "Done"
 
-	def init(rsa_id):
+	def set_entity(self):
+		self.bot_entity = self.client.get_entity(self.bot_name)
+
+	def serve(self):
+		while True:
+			msg = self.get_new_message()
+			if msg.id == self.msg_id:
+				time.sleep(1)
+				continue
+			self.msg_id = msg.id
+			self.execute(msg)
+			msg.edit(msg.message)
+
+	def start(self):
 		self.client.start()
-		receiver = self.client.get_input_entity(self.bot_name)
-		self.client.send_message(receiver, 'connect_cmd\n%s' % self.__get_rsa())
+		self.set_entity()
+		self.serve()
 
+if __name__ == "__main__":
+	cli = Client()
+	try:
+		cli.start()
+	except KeyboardInterrupt:
+		print("\rTerminate")
